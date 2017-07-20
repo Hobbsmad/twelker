cabb4c774a09a8b54c77f6535b4d1dd2444a9d29
Jason Hobbs <jason.hobbs@digisanctum.com>
2017-07-19 23:25:31 -0500

    fixed tether

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require tether
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require tether
 //= require_tree .
