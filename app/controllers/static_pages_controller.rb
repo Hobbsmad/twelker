b44ce6470a47187205c92e117409fe5d3b5cf7e2
Madelyn Hobbs <madelyn.hobbs@gmail.com>
2017-08-15 21:16:09 -0500

    working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def client
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ get "/pages/:page" => "pages#show"
 
 get '/static_pages/about', to: 'static_pages#about', as: :about
 get '/static_pages/contact', to: 'static_pages#contact', as: :contact
+get '/static_pages/client', to: 'static_pages#client', as: :client
 end
