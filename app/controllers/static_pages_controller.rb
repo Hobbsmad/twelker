ef06cf4fcfcd5ecc679117ed718b982dbb0c097e
Jason Hobbs <jason.hobbs@digisanctum.com>
2017-08-25 22:57:27 -0500

    added contact form validations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,6 +12,21 @@ class StaticPagesController < ApplicationController
   end
 
   def contactemail
+    @email = params[:email]
+    @name = params[:name]
+    @title = params[:title]
+    @phone = params[:phone]
+    @subject = params[:subject]
+    if params[:name].empty?
+      gflash :now, :error => "Name cannot be empty"
+      render('contact')
+      return
+    end
+    if params[:email].empty?
+      gflash :now, :error => "Email cannot be empty"
+      render('contact')
+      return
+    end
     ContactusMailer.newcontact(params[:email], params[:title], params[:name], params[:subject], params[:phone], params[:company]).deliver
     redirect_to root_url, gflash: { success: 'Thank you for contacting us!  We will be in touch soon!' }
   end
